0a2da94a6ef959d6dabb1424c7ac07f0a3593512
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2026-02-05 15:31:58 +0100

    flake: update python to 3.14
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- flake.nix
+++ flake.nix
@@ -5,60 +5,65 @@
     futils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-terraform,
-    futils,
-  } @ inputs: let
-    inherit (nixpkgs) lib;
-    inherit (futils.lib) eachDefaultSystem defaultSystems;
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-terraform
+    , futils
+    ,
+    } @ inputs:
+    let
+      inherit (nixpkgs) lib;
+      inherit (futils.lib) eachDefaultSystem defaultSystems;
 
-    nixpkgsFor = lib.genAttrs defaultSystems (system:
-      import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfreePredicate = pkg:
-            builtins.elem (lib.getName pkg) [
-              "vault"
-            ];
+      nixpkgsFor = lib.genAttrs defaultSystems (system:
+        import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfreePredicate = pkg:
+              builtins.elem (lib.getName pkg) [
+                "vault"
+              ];
+          };
+        });
+    in
+    (eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (import nixpkgs-terraform {
+              inherit system;
+              config = {
+                allowUnfreePredicate = pkg:
+                  builtins.elem (lib.getName pkg) [
+                    "terraform"
+                  ];
+              };
+            }).terraform
+            cargo
+            git
+            jq
+            openssl
+            python314
+            rustc
+            shellcheck
+            uv
+            vault
+          ];
+
+          shellHook = ''
+            source ./config.sh
+          '';
+
+          UV_NO_BINARY_PACKAGE = "ruff";
+
+          # See https://github.com/NixOS/nix/issues/318#issuecomment-52986702
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
         };
-      });
-  in (eachDefaultSystem (
-    system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (import nixpkgs-terraform {
-            inherit system;
-            config = {
-              allowUnfreePredicate = pkg:
-                builtins.elem (lib.getName pkg) [
-                  "terraform"
-                ];
-            };
-          }).terraform
-          cargo
-          git
-          jq
-          openssl
-          python313
-          rustc
-          shellcheck
-          uv
-          vault
-        ];
-
-        shellHook = ''
-          source ./config.sh
-        '';
-
-        UV_NO_BINARY_PACKAGE = "ruff";
-
-        # See https://github.com/NixOS/nix/issues/318#issuecomment-52986702
-        LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-      };
-    }
-  ));
+      }
+    ));
 }
