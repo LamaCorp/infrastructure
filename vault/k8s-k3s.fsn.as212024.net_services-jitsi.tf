8dd7e34df8c9ec5f39756c472a896df08beee334
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2026-02-13 14:46:57 +0100

    k8s/services/rocketchat: remove jitsi auth as code, rocketchat doesn't sync it
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- vault/k8s-k3s.fsn.as212024.net_services-jitsi.tf
+++ vault/k8s-k3s.fsn.as212024.net_services-jitsi.tf
@@ -8,18 +8,3 @@ resource "vault_generic_secret" "k8s-k3s-fsn-as212024-net_services-jitsi_jwt-aut
     JWT_APP_SECRET = random_password.k8s-k3s-fsn-as212024-net_services-jitsi_jwt-auth.result
   })
 }
-
-locals {
-  rocketchat_instances = toset([
-    "sdlh",
-    "lama",
-  ])
-}
-
-resource "vault_generic_secret" "k8s-k3s-fsn-as212024-net_services-rocketchat_jitsi-auth" {
-  for_each = local.rocketchat_instances
-  path     = "${vault_mount.k8s-clusters["k3s.fsn.as212024.net"].path}/services-rocketchat-${each.key}/jitsi-auth"
-  data_json = jsonencode({
-    app_secret = random_password.k8s-k3s-fsn-as212024-net_services-jitsi_jwt-auth.result
-  })
-}
