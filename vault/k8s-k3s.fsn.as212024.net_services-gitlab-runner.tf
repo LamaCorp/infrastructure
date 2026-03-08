5ec3c4f286ce0507aaa4f39f7b47b1c798212d3b
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2026-03-08 04:34:10 +0100

    k8s/services/gitlab-runner: remove
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- vault/k8s-k3s.fsn.as212024.net_services-gitlab-runner.tf
+++
@@ -1,8 +0,0 @@
-resource "vault_generic_secret" "k8s-k3s-fsn-as212024-net_services-gitlab-runner_registration_lama-corp" {
-  path         = "${vault_mount.k8s-clusters["k3s.fsn.as212024.net"].path}/services-gitlab-runner/registration/lama-corp"
-  disable_read = true
-  data_json = jsonencode({
-    runner-registration-token = ""
-    runner-token              = "FIXME: get this from GitLab.com"
-  })
-}
