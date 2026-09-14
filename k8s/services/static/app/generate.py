cfd5b9aecf91664e095515415258fcabd65b863a
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2026-09-14 17:38:26 +0200

    k8s/services/static: switch to listenerset
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- k8s/services/static/app/generate.py
+++ k8s/services/static/app/generate.py
@@ -9,7 +9,7 @@ def main():
     root_dir = Path(__file__).parent
     context = yaml.safe_load((root_dir / "context.yml").read_text())
     for file in root_dir.glob("**/*.j2"):
-        env = jinja2.Environment()
+        env = jinja2.Environment(lstrip_blocks=True)
         rendered = env.from_string(file.read_text()).render(**context)
         Path(str(file).removesuffix(".j2")).write_text(rendered)
 
